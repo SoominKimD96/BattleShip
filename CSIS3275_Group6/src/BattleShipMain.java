22354e1cd7f47d7369484d41fb9c46e1c9834f42
SoominKimD96 <77809742+SoominKimD96@users.noreply.github.com>
2022-03-07 16:40:05 -0800

    Add files via upload

---
+++ CSIS3275_Group6/src/BattleShipMain.java
@@ -0,0 +1,27 @@
+import java.awt.EventQueue;
+
+import controller.BattleShipController;
+
+/**
+ * This is the main class to execute BattleShip Application
+ * @author Group 6
+ *
+ */
+public class BattleShipMain {
+
+	/**
+	 * Execute the Application.
+	 * @param args default parameter
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BattleShipController controller = new BattleShipController();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
